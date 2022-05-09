1edf9e924ecd820e81323dabec633afdab222c81
Prabhusns <prabhusns@gmail.com>
2022-05-09 17:41:22 +0530

    Update EmailAlert.java
    Comments

--- src/main/java/statisticker/EmailAlert.java
+++ src/main/java/statisticker/EmailAlert.java
@@ -1,9 +1,12 @@
 package statisticker;
 
+// Email alert class implemets Ialerter interface
 public class EmailAlert implements IAlerter {
-
+	
+	// boolean value
 	public boolean emailSent;
 
+	// implemented interface method
 	@Override
 	public void triggerAlert() {
 		emailSent = true;
