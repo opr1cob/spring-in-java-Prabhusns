59823c0061d546b5509df114c97733c02ed01a3b
Prabhusns <prabhusns@gmail.com>
2022-05-09 15:25:50 +0530

    Update Statistics.java
    Test case 1 - reports avg min and max

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -4,7 +4,17 @@ import java.util.List;
 
 public class Statistics 
 {
-    public static Stats getStatistics(List<___> numbers) {
-        //implement the computation of statistics here
+     public static Stats getStatistics(List<Float> numbers) {
+    	Stats stats = new Stats();
+    	OptionalDouble avg =  numbers.stream().mapToDouble((a) -> a).average();
+    	stats.setAverage(avg.getAsDouble());
+    	
+    	OptionalDouble min =  numbers.stream().mapToDouble((a) -> a).min();
+    	stats.setMin(min.getAsDouble());
+    	
+    	OptionalDouble max =  numbers.stream().mapToDouble((a) -> a).max();
+    	stats.setMax(max.getAsDouble());
+    	
+		return stats;
     }
 }
