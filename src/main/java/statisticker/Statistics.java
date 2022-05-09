a9b3b94d816abc9e2a97e26a8e6e6995771fc4f0
Prabhusns <prabhusns@gmail.com>
2022-05-09 16:00:42 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -1,27 +1,25 @@
 package statisticker;
-
 import java.util.List;
-
-public class Statistics {
-    public static Stats getStatistics(List < Float > numbers) {
-        Stats stats = new Stats();
-        OptionalDouble avg = numbers.stream().mapToDouble((a) - > a).average();
-        stats.setAverage(avg.getAsDouble());
-
-        OptionalDouble min = numbers.stream().mapToDouble((a) - > a).min();
-        stats.setMin(min.getAsDouble());
-
-        OptionalDouble max = numbers.stream().mapToDouble((a) - > a).max();
-        stats.setMax(max.getAsDouble());
-
-        return stats;
+import java.util.OptionalDouble;
+public class Statistics
+{
+    public static Stats getStatistics(List<Float> numbers) {
+    	Stats stats = new Stats();
+    	OptionalDouble avg =  numbers.stream().mapToDouble((a) -> a).average();
+    	stats.setAverage(avg.getAsDouble());
+    	
+    	OptionalDouble min =  numbers.stream().mapToDouble((a) -> a).min();
+    	stats.setMin(min.getAsDouble());
+    	
+    	OptionalDouble max =  numbers.stream().mapToDouble((a) -> a).max();
+    	stats.setMax(max.getAsDouble());
+    	
+		return stats;
     }
-
     static class Stats {
         double min; //minimum value
         double average; // avg value on the list
-        double max; // max value 
-
+        double max; // max value
         // getter and setter
         public double getMin() {
             return min;
@@ -42,5 +40,4 @@ public class Statistics {
             this.max = max;
         }
     }
-
 }
